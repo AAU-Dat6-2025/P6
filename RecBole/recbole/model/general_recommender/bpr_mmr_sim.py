4b7f7d27bca2c71a7205bfc91232c627e837fd5f
Sandra <snro22@student.aau.dk>
2025-04-15 09:41:13 +0200

    small fixes

--- RecBole/recbole/model/general_recommender/bpr_mmr_sim.py
+++ RecBole/recbole/model/general_recommender/bpr_mmr_sim.py
@@ -62,10 +62,16 @@ class BPRMMRSim(GeneralRecommender):
         user_e = self.get_user_embedding(user)
         all_item_e = self.item_embedding.weight
         score = torch.matmul(user_e, all_item_e.transpose(0, 1))
-        print(score.shape)
+        print(score.view(-1))
 
         reranker = MMRReranker(lambda_mmr=0.5, top_k=20, n_items=500)
-        reranked_scores = reranker.rerank(user_e, all_item_e,score)
-        print(reranked_scores)
+        reranked_scores = reranker.rerank(user_e, all_item_e, score)  # List of tensors, one per user
 
-        return reranked_scores
+        updated_score = score.clone()
+
+        # Replace top-500 scores for each user with reranked ones
+        for i, user_scores in enumerate(reranked_scores):
+            updated_score[i, :user_scores.size(0)] = user_scores
+
+
+        return updated_score
